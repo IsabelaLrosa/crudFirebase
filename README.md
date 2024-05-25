09ff358bb3014cdc3683de45c61485f60e8fb14d
IsabelaLrosa <isabelalrosa@outlook.com>
2024-05-24 22:02:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Fatec Itu - Dom Amaury Castanho 📚
+
+# Aula de aprimoramento 
+>> Matéria: Programação para a internet 
+>> Professor: Ricardo Roberto Leme 
+
+# Tecnologias Utilizadas 🤖
+HTML, CSS, LocalStorage e Java Script
+
+# Contato ✉️
+ISABELA LEMES ROSA [IsabelaLrosa](isabelalrosa@outlook.com)
---
+++ js/firebase/firebase.js
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2023 Prof. Ms. Ricardo Leme All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 'use strict' //modo estrito
+
+/********************************************** 
+* Renomeie este arquivo para firebase.js!
+***********************************************/
+
+// Cole as informações do seu RealTime Database do Firebase abaixo:
+const firebaseConfig = {
+  apiKey: "AIzaSyCGr6fxbgVDV64bxt2rX731KeC9J88jSws",
+  authDomain: "crud-firebase-isabela.firebaseapp.com",
+  projectId: "crud-firebase-isabela",
+  storageBucket: "crud-firebase-isabela.appspot.com",
+  messagingSenderId: "121026807172",
+  appId: "1:121026807172:web:e7bff7e5a02b5d493089d1"
+};
+
+
+/*
+* Nas regras do Realtime Database, informe:
+* {
+  "rules": {
+    "clientes":{
+    ".read": "auth != null",
+    ".write": "auth != null"
+  },
+    "usuarios":{
+    ".read": "auth != null",
+    ".write": "auth != null"
+  }
+ }
+}
+*/
+
+// Inicializando o Firebase
+firebase.initializeApp(firebaseConfig);
+// Crie uma referência para o Realtime Database do Firebase
+const database = firebase.database();
+// Crie uma referência para o armazenamento do Firebase
+const storageRef = firebase.storage().ref();
